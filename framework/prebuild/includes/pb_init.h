8040e667c7656a529de0950ae5fd1f536717f3e2
SudoQuack <guillaume.astesana@gmail.com>
2025-11-10 10:30:36 +0100

    Revert "prebuilder working"
    This reverts commit abb41a3f4f673740de978de1fe01aeed2d5e5040.

--- framework/prebuild/includes/pb_init.h
+++
@@ -1,8 +0,0 @@
-#ifndef ULTIMATE_PREBUILD_INIT_H
-# define ULTIMATE_PREBUILD_INIT_H
-
-# include "pb_types.h"
-
-bool	init_run(t_run *run, char **argv);
-
-#endif
--- framework/prebuild/includes/pb_parse.h
+++
@@ -1,9 +0,0 @@
-#ifndef ULTIMATE_PREBUILD_ARGS_H
-# define ULTIMATE_PREBUILD_ARGS_H
-
-# include "pb_types.h"
-
-bool	parse_target(t_target *target, char *raw);
-bool	parse_args(t_target *target, const char *str, t_string *output);
-
-#endif
